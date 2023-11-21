80f9376d9e0dd2bd65b8b0b98f059ac17a4e4831
Rodrigo Fontes <rodrigofontes377@gmail.com>
2023-11-21 17:28:26 -0300

    Merge branch 'dev'

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,57 @@ navLinks.forEach((link) => {
     link.style.color = ""; // aqui você pode definir a cor padrão dos textoss
   });
 });
+// Código para o primeiro carrossel (minPerSlide = 4)
+let itemsFirstCarousel = document.querySelectorAll(
+  "#carouselExampleControls .carousel-item"
+);
+console.log("file: bootstrap.html:174 - items:", itemsFirstCarousel);
 
-let items = document.querySelectorAll(".carousel .carousel-item");
-console.log("file: bootstrap.html:174 - items:", items);
-
-items.forEach((el) => {
+itemsFirstCarousel.forEach((el) => {
   const minPerSlide = 4;
   let next = el.nextElementSibling;
+
   for (let i = 1; i < minPerSlide; i++) {
     if (!next) {
-      //   wrap carousel by using first child
-      next = items[0];
+      // Wrap carousel by using first child
+      next = itemsFirstCarousel[0];
     }
+
     let cloneChild = next.cloneNode(true);
     el.appendChild(cloneChild.children[0]);
     next = next.nextElementSibling;
   }
+});
+
+let itemsSecondCarousel = document.querySelectorAll(
+  "#organizacao .carousel-item"
+);
+
+itemsSecondCarousel.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+
+  for (let i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // Se não houver próximo slide, volte ao primeiro slide
+      next = itemsSecondCarousel[0];
+    }
+
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+function openImageModal(imagePath) {
+  let modalImage = document.getElementById("modalImage");
+  modalImage.src = imagePath;
+  $("#imageModal").modal("show");
+}
+
+// Botão  fale conosco tela inicial
+document.addEventListener("DOMContentLoaded", function () {
+  const btnFaleConosco = document.querySelector("#btnFaleConosco");
+  btnFaleConosco.addEventListener("click", () => {
+    window.open("https://www.instagram.com/acarte1111/");
+  });
 });
